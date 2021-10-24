b4f94571819264d3027c3436d06667fefb6acb79
Dmitry Igumnov <53914429+ehnat0n@users.noreply.github.com>
2021-10-24 00:38:15 -0600

    added test - check specific address on 1st page

--- mocha-swd/test/google-search.js
+++ mocha-swd/test/google-search.js
@@ -1,9 +1,12 @@
 import {Builder, By, Key, until} from "selenium-webdriver";
+import {strict as assert} from "assert";
 
-let searchQuery = "Portnov School";
+let searchQuery = "Web Driver";
+let desiredAddress = "https://www.selenium.dev/documentation/webdriver/";
+let foundOnFirstPage = false;
 let browserList = ["chrome", "firefox"];
 
-describe("Test run for '" + searchQuery + "' query.", () => {
+describe("Test run for '" + searchQuery + "' query", () => {
     for (let i = 0; i < browserList.length; i++) {
       let driver;
       let browserName = browserList[i];
@@ -19,14 +22,14 @@ describe("Test run for '" + searchQuery + "' query.", () => {
           await driver.quit();
         });
 
-        it("Should be able to complete the search and verify the title.", async () => {
+        it("Should be able to complete the search and verify the title", async () => {
           let locatorXPath = By.xpath("//*[@name='q']");
           await driver.findElement(locatorXPath).sendKeys(searchQuery, Key.RETURN);
 
           await driver.wait(until.titleIs(searchQuery + " - Google Search"), 10000);
         });
 
-        it("Should list all search results from the first page.", async () => {
+        it("Should find desired address on the first page and list all results in order", async () => {
           /**
            * Google search results are under <div id="main">...<div id="center_col">...<div id="search">...<div class="g">...
            * There is a link with <a> tag and a title with <h3> tag.
@@ -35,13 +38,25 @@ describe("Test run for '" + searchQuery + "' query.", () => {
           await driver.wait(until.elementLocated(resultsLocator), 10000); // to make sure we got first result loaded
           let resultsList = await driver.findElements(resultsLocator);
 
+          let desiredAddressLabel = "Desired address was not found on the first page";
+
           for (let i = 1; i <= resultsList.length; i++) {
             let eachResult = resultsList[i-1];
             let myLinkText = await eachResult.findElement(By.xpath(".//a")).getAttribute("href");
             let myCaption = await eachResult.findElement(By.xpath(".//h3")).getText();
 
-            console.log(i + ". " + myCaption + ". Link: " + myLinkText);
+            let iterationLabel = " " + i + ". ";
+
+            if (myLinkText === desiredAddress) {
+              foundOnFirstPage = true;
+              desiredAddressLabel = "Desired address - search result number: " + i;
+              iterationLabel = "[" + i + "]. ";
+            }
+
+            console.log(iterationLabel + myCaption + ". Link: " + myLinkText);
           }
+          console.log(desiredAddressLabel);
+          assert.equal(foundOnFirstPage, true);
         });
       });
     }
